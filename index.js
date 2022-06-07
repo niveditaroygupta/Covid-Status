5bd1cda55928da2d66f34b488589a2ebfcebd2cc
NIVEDITA ROY GUPTA <royguptanivedita@yahoo.com>
2022-06-07 18:26:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$.ajax({
+    type:'get' ,
+    url:'https://api.covid19api.com/summary',
+    success : function(response)
+    {
+        console.log(response.Countries)
+        for(var i=0; i<response.Countries.length; i++)
+        {
+            var recovered = response.Countries[i].TotalConfirmed - response.Countries[i].TotalDeaths
+            var tablerow = `<tr><td>${response.Countries[i].Country}</td><td>${response.Countries[i].TotalConfirmed}</td><td>${response.Countries[i].NewConfirmed}</td><td>${recovered}</td><td>${response.Countries[i].TotalDeaths}</td></tr>`
+            $('#tbody').append(tablerow)
+        }
+        $('#covidtable').DataTable()
+      
+    },
+    error: function(error)
+    {
+        console.log(error)
+    }
+})
